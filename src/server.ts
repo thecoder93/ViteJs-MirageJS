51c5c3686c84852b307af8b1c863ba78953b0e7d
Hanslukas <gl.lamanna@gmail.com>
2021-10-05 23:44:01 +0200

    add movie model and movie by id

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,24 @@
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 
 export function run () {
     return createServer({
+
+        models: {
+            movie: Model, //Consente di utilizzare il DB in memory che rende dinamici gli handler delle route.
+          },
+
         routes() {
+            this.get("/movies", (schema, request) => {
+                // @ts-ignore
+                return schema.movies.all()
+              });
+
+              this.get("/movies/:id", (schema, request) => { //posso chiamare la get per id e restituire il singolo film
+                let id = request.params.id
+                // @ts-ignore
+                return schema.movies.find(id)
+              })
+
             this.get('/movies', () => {
                 return {
                     movies: [
@@ -24,7 +40,18 @@ export function run () {
                     ],
                     total: 3
                 }
-            })
-        }
+            },
+            {timing: 4000},
+            )
+        },
+        seeds(server) { //riempiamo il nostro db con seeds
+            //server.create mi aggiugne automaticamente un id ed effettua un autoincrement
+            // @ts-ignore
+            server.create("movie", { title: "Inception", year: 2010 })
+            // @ts-ignore
+            server.create("movie", { title: "Interstellar", year: 2014 })
+            // @ts-ignore
+            server.create("movie", { title: "Dunkirk", year: 2017 })
+          },
     })
 }
